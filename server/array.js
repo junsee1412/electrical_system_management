9042cf2cfb4f435cac3634c24172d5a543066523
junsee <dat10319@gmail.com>
2022-05-30 19:46:59 +0700

    add .gitignore :v

--- server/array.js
+++
@@ -1,7 +0,0 @@
-let a = [1,2,3,4,5]
-let b = [1,6,2,7]
-
-// if (b.filter(x => !a.includes(x)).length != 0) {
-//     console.log(b.filter(x => !a.includes(x)));
-// }
-console.log(a.filter(x => !b.includes(x)))
